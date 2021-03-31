4404e4ee9581728f3087a9c2a460dc08e3507221
Eduardo Tongson <propolice@gmail.com>
2021-03-31 15:52:39 +0800

    tests: Split tests so it is easier to see where it fails.

--- tests/json.lua
+++ tests/json.lua
@@ -1,18 +1,49 @@
 return function()
   local T = require 'test'
-  T.is_table(json)
-  T.is_function(json.encode)
-  T.is_function(json.decode)
-  T.equal(json.encode(true), 'true')
-  T.equal(json.encode(1), '1')
-  T.equal(json.encode(-10), '-10')
-  T.equal(json.encode(nil), 'null')
-  T.equal(json.encode({}), '[]')
-  T.equal(json.encode({1, 2, 3}), '[1,2,3]')
-  T.error(json.encode, 'sparse array', ({1, 2, [10] = 3}))
-  T.error(json.encode, 'mixed or invalid key types', {1, 2, 3, name = 'Tim'})
-  T.error(json.encode, 'mixed or invalid key types', {name='Tim', [false]=123})
-  do
+  T['json'] = function()
+    T.is_table(json)
+  end
+  T['json.encode'] = function()
+    T.is_function(json.encode)
+  end
+  T['json.decode'] = function()
+    T.is_function(json.decode)
+  end
+  T['json.encode TRUE'] = function()
+    T.equal(json.encode(true), 'true')
+  end
+  T['json.encode NUMBER'] = function()
+    T.equal(json.encode(1), '1')
+  end
+  T['json.encode SIGNED NUMBER'] = function()
+    T.equal(json.encode(-10), '-10')
+  end
+  T['json.encode NIL'] = function()
+    T.equal(json.encode(nil), 'null')
+  end
+  T['json.encode EMPTY TABLE'] = function()
+    T.equal(json.encode({}), '[]')
+  end
+  T['json.encode SEQUENCE'] = function()
+    T.equal(json.encode({1, 2, 3}), '[1,2,3]')
+  end
+  T['json.encode SEQUENCE'] = function()
+    local a = {}
+    for i=1, 5 do
+      a[i] = i
+    end
+    T.equal(json.encode(a), "[1,2,3,4,5]")
+  end
+  T['json.encode SPARSE TABLE'] = function()
+    T.error(json.encode, 'sparse array', ({1, 2, [10] = 3}))
+  end
+  T['json.encode MIXED TABLE #1'] = function()
+    T.error(json.encode, 'mixed or invalid key types', {1, 2, 3, name = 'Tim'})
+  end
+  T['json.encode MIXED TABLE #2'] = function()
+    T.error(json.encode, 'mixed or invalid key types', {name='Tim', [false]=123})
+  end
+  T['json.decode ARRAY'] = function()
     local obj = {"a",1,"b",2,"c",3}
     local jsonStr = json.encode(obj)
     local jsonObj = json.decode(jsonStr)
@@ -20,16 +51,20 @@ return function()
       T.equal(obj[i], jsonObj[i])
     end
   end
-  do
+  T['json.decode OBJECT'] = function()
     local obj = {name="Tim",number=12345}
     local jsonStr = json.encode(obj)
     local jsonObj = json.decode(jsonStr)
     T.equal(obj.name, jsonObj.name)
     T.equal(obj.number, jsonObj.number)
   end
-  T.is_nil(json.decode("null"))
-  T.equal(json.decode(json.encode({person={name = "tim",}})).person.name, 'tim')
-  do
+  T['json.decode NULL'] = function()
+    T.is_nil(json.decode("null"))
+  end
+  T['json.decode NESTED OBJECT'] = function()
+    T.equal(json.decode(json.encode({person={name = "tim",}})).person.name, 'tim')
+  end
+  T['json.encode INVALID TABLE'] = function()
     local obj = {
       abc = 123,
       def = nil,
@@ -39,12 +74,5 @@ return function()
     }
     obj.obj2 = obj2
     T.is_nil(json.encode(obj))
-  end
-  do
-    local a = {}
-    for i=1, 5 do
-      a[i] = i
-    end
-    T.equal(json.encode(a), "[1,2,3,4,5]")
   end
 end
