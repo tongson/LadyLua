094aff8e49055ff672185238425be5df57b6fe43
Eduardo Tongson <propolice@gmail.com>
2021-03-31 21:36:33 +0800

    tests: Test exec.command CWD/Dir.

--- tests/exec.lua
+++ tests/exec.lua
@@ -1,11 +1,23 @@
 return function()
   local T = require 'test'
+  local D = '/tmp/exec.command'
+  local F = '/tmp/exec.command/file'
   T['exec.command SIMPLE'] = function()
-    T.is_true(fs.mkdir('/tmp/exec.command'))
-    T.is_true(exec.command('/bin/touch', {'/tmp/exec.command/file'}))
-    T.is_true(fs.isfile('/tmp/exec.command/file'))
-    T.is_true(exec.command('/usr/bin/rm', {'/tmp/exec.command/file'}))
-    T.is_false(fs.isfile('/tmp/exec.command/file'))
-    T.is_true(fs.rmdir('/tmp/exec.command'))
+    T.is_true(fs.mkdir(D))
+    T.is_true(exec.command('/bin/touch', {F}))
+    T.is_true(fs.isfile(F))
+    T.is_true(exec.command('/usr/bin/rm', {F}))
+    T.is_false(fs.isfile(F))
+    T.is_true(fs.rmdir(D))
+  end
+  T['exec.command CWD'] = function()
+    T.is_true(fs.mkdir(D))
+    local a = {'/tmp/exec.command/file'}
+    local d = '/tmp/exec.command'
+    T.is_true(exec.command('/bin/touch', {'file'}, nil, D))
+    T.is_true(fs.isfile(F))
+    T.is_true(exec.command('/usr/bin/rm', {F}))
+    T.is_false(fs.isfile(F))
+    T.is_true(fs.rmdir(D))
   end
 end
