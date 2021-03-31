9661fd31ef649c2271b9ab52e18cef07bc67283c
Eduardo Tongson <propolice@gmail.com>
2021-03-31 16:40:50 +0800

    tests: Better arrangement.

--- test.lua
+++ test.lua
@@ -15,6 +15,10 @@ T.register_assert('error', errstr)
 
 T['built-in => json'] = dofile 'tests/json.lua'
 T['built-in => fs'] = dofile 'tests/fs.lua'
+T["built-in => os.hostname"] = function()
+  T.is_function(os.hostname)
+  T.is_string(os.hostname())
+end
 T["global => pi"] = function()
   T.is_function(pi)
   T.is_number(pi(300))
@@ -28,11 +32,4 @@ T["module => kikito/inspect"] = function()
   T.is_function(inspect.inspect)
   T.equal(inspect(t), '{ 1, 2 }')
 end
-T["built-in => os.hostname"] = function()
-  T.is_function(os.hostname)
-  T.is_string(os.hostname())
-end
-
-
-
 T.summary()
