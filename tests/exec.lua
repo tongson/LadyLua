59fc998d31dfa2544ef72d6a5eed70778d94bd94
Eduardo Tongson <propolice@gmail.com>
2021-04-01 14:13:10 +0800

    tests: exec.ctx.

--- tests/exec.lua
+++ tests/exec.lua
@@ -32,4 +32,48 @@ return function()
     local s = string.find(o, 'gg', 1, true)
     T.is_number(s)
   end
+  -----
+  T['exec.ctx SIMPLE'] = function()
+    T.is_true(fs.mkdir(D))
+    local touch = exec.ctx('/bin/touch')
+    local t = touch(F)
+    T.is_true(t)
+    T.is_true(fs.isfile(F))
+    local rm = exec.ctx('/usr/bin/rm')
+    local r = rm(F)
+    T.is_true(r)
+    T.is_false(fs.isfile(F))
+    T.is_true(fs.rmdir(D))
+  end
+  T['exec.ctx CWD'] = function()
+    T.is_true(fs.mkdir(D))
+    local a = {'/tmp/exec.command/file'}
+    local d = '/tmp/exec.command'
+    local touch = exec.ctx('/bin/touch')
+    touch.cwd = D
+    local t = touch'file'
+    T.is_true(t)
+    T.is_true(fs.isfile(F))
+    local rm = exec.ctx('/usr/bin/rm')
+    local r = rm(F)
+    T.is_true(r)
+    T.is_false(fs.isfile(F))
+    T.is_true(fs.rmdir(D))
+  end
+  T['exec.ctx ENV'] = function()
+    local env = exec.ctx'/usr/bin/env'
+    env.env = {'EXEC=ok'}
+    local r, o = env()
+    T.is_true(r)
+    local s = string.find(o, 'EXEC=ok', 1, true)
+    T.is_number(s)
+  end
+  T['exec.ctx STDIN'] = function()
+    local sed = exec.ctx('/usr/bin/sed')
+    sed.stdin = 'ss'
+    local r, o = sed('s|ss|gg|')
+    T.is_true(r)
+    local s = string.find(o, 'gg', 1, true)
+    T.is_number(s)
+  end
 end
