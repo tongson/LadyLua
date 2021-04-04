786acf88490f843a91894194780396eefdc23504
Eduardo Tongson <propolice@gmail.com>
2021-04-04 12:21:54 +0800

    It is not clear if the generated table is a list or map.

--- lua/string.lua
+++ lua/string.lua
@@ -5,7 +5,7 @@ string.append = function(str, a)
   return F("%s\n%s", str, a)
 end
 
-string.line_to_table = function(str)
+string.line_to_list = function(str)
   local tbl = {}
   if not str then
     return tbl
@@ -16,7 +16,7 @@ string.line_to_table = function(str)
   return tbl
 end
 
-string.word_to_table = function(str)
+string.word_to_list = function(str)
   local t = {}
   for s in gmatch(str, "%w+") do
     t[#t + 1] = s
@@ -24,7 +24,7 @@ string.word_to_table = function(str)
   return t
 end
 
-string.to_table = function(str)
+string.to_list = function(str)
   local t = {}
   for s in gmatch(str, "%S+") do
     t[#t + 1] = s
