102e4642ce9a185878abd36e83d5d7e6617e76d3
Eduardo Tongson <propolice@gmail.com>
2021-04-04 19:34:16 +0800

    Tests for table patches.

--- test.lua
+++ test.lua
@@ -12,6 +12,7 @@ local errstr = function(tested, str, ...)
 end
 T.register_assert('error', errstr)
 
+T['built-in global => table'] = dofile 'tests/table.lua'
 T['built-in global => string'] = dofile 'tests/string.lua'
 T['built-in global => fmt'] = dofile 'tests/fmt.lua'
 T["module => html"] = dofile 'tests/html.lua'
