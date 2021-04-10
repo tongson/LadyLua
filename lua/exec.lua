da52b53396ec95ec3cddd02e47206059f7d4a669
Eduardo Tongson <propolice@gmail.com>
2021-04-10 13:21:56 +0800

    Pretty printing of output and split STDOUT and STDERR.

--- lua/exec.lua
+++ lua/exec.lua
@@ -26,10 +26,20 @@ exec.cmd = function(exe)
       args = a
     end
     local r, so, se = exec.command(exe, args, set.env, set.cwd, set.stdin)
+    local pretty_prefix = function(header, prefix, str)
+      if string.len(str) > 0 then
+        local replacement = string.format('\n %s > ', prefix)
+        str = string.gsub(str, "\n", replacement)
+        return string.format("%s\n %s >\n %s > %s\n", header, prefix, prefix, str)
+      else
+        return ""
+      end
+    end
     if set.errexit == true and r == nil then
       local err = set.error or 'execution failed'
-      return fmt.panic('exec.cmd: `%s`=>  "%s"\nstdout=>\n  %s\nstderr=>\n  %s\n',
-        exe, err, so, se)
+      local header = string.format('exec.cmd: `%s` => "%s"', exe, err)
+      fmt.print('%s', pretty_prefix(header, 'stdout', so))
+      return fmt.panic('%s', pretty_prefix(header, 'stderr', se))
     else
       return true, so, se
     end
