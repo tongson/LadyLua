b5031b57c066e2d1531e8f941545289deeb518c0
Eduardo Tongson <propolice@gmail.com>
2021-04-12 12:59:29 +0800

    Working podman and start-mariadb code.

--- scripts/start-mariadb/.files/__ll/modules/podman.lua
+++ scripts/start-mariadb/.files/__ll/modules/podman.lua
@@ -5,7 +5,6 @@ local panic = function(ret, msg, tbl)
     os.exit(1)
   end
 end
-local json = require 'json'
 local podman = exec.cmd 'podman'
 
 local pull = function(u, t)
@@ -20,6 +19,7 @@ local pull = function(u, t)
   })
 end
 local id = function(u, t)
+  local json = require 'json'
   local r, so, se = podman('images --format json')
   panic(r, 'unable to list images', {
     what = 'podman';
@@ -38,17 +38,24 @@ local id = function(u, t)
     end
   end
 end
-local start = function(name, unit, iid)
+local start = function(name, unit, cpus, iid)
   local systemctl = exec.cmd 'systemctl'
   systemctl('disable --now %s.service', name)
   local fname = string.format('/etc/systemd/system/%s.service', name)
-  local funit, changed = string.gsub(unit, '__ID__', iid)
-  panic((changed == 1), 'unable to interpolate', {
+  local changed
+  unit, changed = string.gsub(unit, '__ID__', iid)
+  panic((changed == 1), 'unable to interpolate image ID', {
     what = 'string.gsub';
     changed = false;
     to = iid;
   })
-  panic(fs.write(fname, funit), 'unable to write unit', {
+  unit, changed = string.gsub(unit, '__CPUS__', cpus)
+  panic((changed == 1), 'unable to interpolate cpuset-cpus', {
+    what = 'string.gsub';
+    changed = false;
+    to = cpus;
+  })
+  panic(fs.write(fname, unit), 'unable to write unit', {
     what = 'fs.write';
     file = fname;
   })
@@ -61,9 +68,29 @@ local start = function(name, unit, iid)
     stderr = se;
   })
 end
-
+local generate_password_file = function(path)
+  local password = require 'password'
+  local util = require 'util'
+  local p = password.generate(16, 2, 2, true, false)
+  if p == nil or string.len(p) == 0 then
+    panic(nil, 'unable to generate password', {
+      what = 'password.generate'
+    })
+  end
+  local dir = util.path_split(path)
+  util.path_apply(fs.mkdir, dir)
+  panic(fs.isdir(dir), 'unable to create directory', {
+    what = 'fs.mkdir';
+    dir = dir;
+  })
+  panic(fs.write(path, p), 'unable to write password', {
+    what = 'fs.write';
+    path = path;
+  })
+end
 return {
   pull = pull;
   id = id;
   start = start;
+  generate_password_file = generate_password_file;
 }
--- scripts/start-mariadb/.files/__ll/start-mariadb.lua
+++ scripts/start-mariadb/.files/__ll/start-mariadb.lua
@@ -2,11 +2,67 @@ package.path = '/__ll/modules/?.lua'
 local podman = require 'podman'
 
 local UNIT = [==[
+[Unit]
+Description=MariaDB Container
 
+[Service]
+RestartSec=5
+KillMode=none
+Restart=always
+SystemCallArchitectures=native
+MemoryDenyWriteExecute=yes
+LockPersonality=yes
+NoNewPrivileges=yes
+RemoveIPC=yes
+DevicePolicy=closed
+PrivateTmp=yes
+PrivateNetwork=false
+PrivateDevices=yes
+ProtectKernelModules=yes
+ProtectSystem=full
+ProtectHome=yes
+#ProtectKernelTunables=yes
+ProtectKernelLogs=yes
+ProtectClock=yes
+RestrictRealtime=yes
+#RestrictSUIDSGID=yes
+RestrictAddressFamilies=AF_INET AF_UNIX
+SystemCallFilter=~bpf process_vm_writev process_vm_readv perf_event_open kcmp lookup_dcookie move_pages swapon swapoff userfaultfd unshare
+SystemCallFilter=~@cpu-emulation @debug @module @obsolete @keyring @clock @raw-io @clock @swap @reboot
+LimitMEMLOCK=infinity
+LimitNOFILE=infinity
+LimitNPROC=infinity
+ExecStartPre=-/usr/bin/mkdir -p /srv/podman/mariadb/data
+ExecStartPre=-/usr/bin/chown -R 999:999 /srv/podman/mariadb/data
+ExecStartPre=-/usr/bin/chmod 0600 /srv/podman/mariadb/password
+ExecStartPre=-/usr/bin/podman stop -i mariadb
+ExecStartPre=-/usr/bin/podman rm -i -v -f mariadb
+ExecStop=/usr/bin/podman stop -t 12 mariadb
+ExecStopPost=-/usr/bin/podman rm -i -v -f mariadb
+ExecStart=/usr/bin/podman run --name mariadb \
+--hostname mariadb --network host \
+--cap-drop all \
+--cap-add setgid \
+--cap-add setuid \
+--cap-add dac_read_search \
+-e "MYSQL_ROOT_PASSWORD_FILE=/etc/mysql/password" \
+-e MALLOC_ARENA_MAX=1 \
+--ulimit memlock=-1:-1 \
+--ulimit nofile=65536:65536 \
+--ulimit nproc=65536:65536 \
+--memory 0 \
+--cpuset-cpus __CPUS__ \
+-v /srv/podman/mariadb/data:/var/lib/mysql:rw \
+-v /srv/podman/mariadb/password:/etc/mysql/password \
+__ID__ --character-set-server=utf8mb4, --collation-server=utf8mb4_unicode_ci --wait_timeout=28800 --log-warnings=0 --port=3306
+
+[Install]
+WantedBy=multi-user.target
 ]==]
 local NAME = 'mariadb'
 local URL  = 'docker://docker.io/library/mariadb'
 local TAG  = '10.5'
+local CPUS = '1'
 local always_update = false
 
 local id = podman.id(URL, TAG)
@@ -14,5 +70,5 @@ if not id or always_update then
   podman.pull(URL, TAG)
   id = podman.id(URL, TAG)
 end
-podman.start(NAME, UNIT, id)
-
+podman.generate_password_file('/srv/podman/mariadb/password')
+podman.start(NAME, UNIT, CPUS, id)
