f672b96c5de7fa870fd5232fa7370beaf58c2bab
Eduardo Tongson <propolice@gmail.com>
2021-04-13 17:32:11 +0800

    Process through stylua.

--- lua/exec.lua
+++ lua/exec.lua
@@ -1,65 +1,68 @@
 exec.ctx = function(exe)
-  local set = {}
-  return setmetatable(set, {__call = function(_, args)
-    args = args or {}
-    return exec.command(exe, args, set.env, set.cwd, set.stdin)
-  end})
+	local set = {}
+	return setmetatable(set, {
+		__call = function(_, args)
+			args = args or {}
+			return exec.command(exe, args, set.env, set.cwd, set.stdin)
+		end,
+	})
 end
 
 exec.cmd = function(exe)
-  local format, len, gsub, gmatch =
-    string.format, string.len, string.gsub, string.gmatch
-  local set = {}
-  return setmetatable(set, {__call = function(_, a, ...)
-    local args
-    if a and type(a) == 'string' then
-      local ergs = format(a, ...)
-      args = {}
-      for k in gmatch(ergs, "%S+") do
-        args[#args+1] = k
-      end
-    end
-    if a and type(a) == 'table' then
-      args = a
-    end
-    local r, so, se = exec.command(exe, args, set.env, set.cwd, set.stdin)
-    local pretty_prefix = function(header, prefix, str)
-      if len(str) > 0 then
-        local replacement = format('\n %s > ', prefix)
-        str = gsub(str, "\n", replacement)
-        return format("%s\n %s >\n %s > %s\n", header, prefix, prefix, str)
-      else
-        return ""
-      end
-    end
-    if set.errexit == true and r == nil then
-      local err = set.error or 'execution failed'
-      local header = format('exec.cmd: `%s` => "%s"', exe, err)
-      if len(so)<1 and len(se)<1 then
-        return fmt.panic('%s\n', header)
-      else
-        fmt.print('%s', pretty_prefix(header, 'stdout', so))
-        return fmt.panic('%s', pretty_prefix(header, 'stderr', se))
-      end
-    else
-      return r, so, se
-    end
-  end})
+	local format, len, gsub, gmatch =
+		string.format, string.len, string.gsub, string.gmatch
+	local set = {}
+	return setmetatable(set, {
+		__call = function(_, a, ...)
+			local args
+			if a and type(a) == 'string' then
+				local ergs = format(a, ...)
+				args = {}
+				for k in gmatch(ergs, '%S+') do
+					args[#args + 1] = k
+				end
+			end
+			if a and type(a) == 'table' then
+				args = a
+			end
+			local r, so, se = exec.command(exe, args, set.env, set.cwd, set.stdin)
+			local pretty_prefix = function(header, prefix, str)
+				if len(str) > 0 then
+					local replacement = format('\n %s > ', prefix)
+					str = gsub(str, '\n', replacement)
+					return format('%s\n %s >\n %s > %s\n', header, prefix, prefix, str)
+				else
+					return ''
+				end
+			end
+			if set.errexit == true and r == nil then
+				local err = set.error or 'execution failed'
+				local header = format('exec.cmd: `%s` => "%s"', exe, err)
+				if len(so) < 1 and len(se) < 1 then
+					return fmt.panic('%s\n', header)
+				else
+					fmt.print('%s', pretty_prefix(header, 'stdout', so))
+					return fmt.panic('%s', pretty_prefix(header, 'stderr', se))
+				end
+			else
+				return r, so, se
+			end
+		end,
+	})
 end
 
-exec.run = setmetatable({},
-  {__index =
-    function (_, exe)
-      return function(...)
-        local args
-        if not (...) then
-          args = {}
-        elseif type(...) == "table" then
-          args = ...
-        else
-          args = {...}
-        end
-        return exec.command(exe, args)
-      end
-    end
-  })
+exec.run = setmetatable({}, {
+	__index = function(_, exe)
+		return function(...)
+			local args
+			if not (...) then
+				args = {}
+			elseif type(...) == 'table' then
+				args = ...
+			else
+				args = { ... }
+			end
+			return exec.command(exe, args)
+		end
+	end,
+})
--- lua/redis.lua
+++ lua/redis.lua
@@ -1,39 +1,39 @@
-local redis = require 'redis'
+local redis = require('redis')
 package.loaded['redis'] = nil
 local loader = function()
-  redis.new = function (addr, password, db)
-    addr = addr or '127.0.0.1:6379'
-    password = password or ''
-    db = db or 0
-    local client = redis.client(addr, password, db)
-    return {
-      hset = function(key, fv)
-        return redis.hset(client, key, fv)
-      end,
-      hget = function(key, field)
-        return redis.hget(client, key, field)
-      end,
-      hdel = function(key, field)
-        return redis.hdel(client, key, field)
-      end,
-      get = function(key)
-        return redis.get(client, key)
-      end,
-      del = function(key)
-        return redis.del(client, key)
-      end,
-      incr = function(key)
-        return redis.incr(client, key)
-      end,
-      set = function(key, value, exp)
-        exp = exp or 0
-        return redis.set(client, key, value, exp)
-      end,
-      close = function()
-        return redis.close(client)
-      end,
-    }
-  end
-  return redis
+	redis.new = function(addr, password, db)
+		addr = addr or '127.0.0.1:6379'
+		password = password or ''
+		db = db or 0
+		local client = redis.client(addr, password, db)
+		return {
+			hset = function(key, fv)
+				return redis.hset(client, key, fv)
+			end,
+			hget = function(key, field)
+				return redis.hget(client, key, field)
+			end,
+			hdel = function(key, field)
+				return redis.hdel(client, key, field)
+			end,
+			get = function(key)
+				return redis.get(client, key)
+			end,
+			del = function(key)
+				return redis.del(client, key)
+			end,
+			incr = function(key)
+				return redis.incr(client, key)
+			end,
+			set = function(key, value, exp)
+				exp = exp or 0
+				return redis.set(client, key, value, exp)
+			end,
+			close = function()
+				return redis.close(client)
+			end,
+		}
+	end
+	return redis
 end
 package.preload['redis'] = loader
