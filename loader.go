31ee869a5e8e6ca87c41098a53c59a82adc2d761
Eduardo Tongson <propolice@gmail.com>
2021-04-18 18:50:25 +0800

    Still under the main package.

--- loader.go
+++ loader.go
@@ -9,20 +9,20 @@ import (
 //go:embed lua/*
 var luaSrc embed.FS
 
-func LuaLoader(L *lua.LState, mod string) lua.LValue {
+func luaLoader(L *lua.LState, mod string) lua.LValue {
 	src, _ := luaSrc.ReadFile(fmt.Sprintf("lua/%s.lua", mod))
 	fn, _ := L.LoadString(string(src))
 	return fn
 }
 
-func PatchLoader(L *lua.LState, mod string) {
+func patchLoader(L *lua.LState, mod string) {
 	src, _ := luaSrc.ReadFile(fmt.Sprintf("lua/%s.lua", mod))
 	fn, _ := L.LoadString(string(src))
 	L.Push(fn)
 	L.PCall(0, 0, nil)
 }
 
-func GlobalLoader(L *lua.LState, mod string) {
+func globalLoader(L *lua.LState, mod string) {
 	L.SetGlobal(mod, L.NewTable())
 	src, _ := luaSrc.ReadFile(fmt.Sprintf("lua/%s.lua", mod))
 	fn, _ := L.LoadString(string(src))
