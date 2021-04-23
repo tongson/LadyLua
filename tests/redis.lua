495eac40f4e0a06d1433ce154d2b7da8f4c8d5b7
Eduardo Tongson <propolice@gmail.com>
2021-04-23 17:40:58 +0800

    Mark as method.

--- tests/redis.lua
+++ tests/redis.lua
@@ -209,7 +209,7 @@ local redis_hget = function()
 	T.equal(rdb:del("ll_hget"), 1)
 end
 --#
---# == *hdel*(_String_, _String_) -> _Number_
+--# == *:hdel*(_String_, _String_) -> _Number_
 --# Removes the specified fields from the hash stored at key. Specified fields that do not exist within this hash are ignored. If key does not exist, it is treated as an empty hash and this command returns 0.
 --#
 --# === Arguments
