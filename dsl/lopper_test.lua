92e9f61154df273537af60b158b4fc2cc9650982
Eduardo Tongson <propolice@gmail.com>
2021-04-24 17:07:39 +0800

    Only NOTIFY() at start and end.

--- dsl/lopper_test.lua
+++ dsl/lopper_test.lua
@@ -1,7 +1,7 @@
 require("lopper")
 T = require("test")
 
-NOTIFY("start CMD test...")
+NOTIFY("start Lopper tests...")
 sh = CMD("sh")
 sh.cwd = "/tmp"
 sh("-c", "touch CMD")
@@ -9,9 +9,7 @@ T["CMD"] = function()
 	T.is_true(fs.isfile("/tmp/CMD"))
 	os.remove("/tmp/CMD")
 end
-NOTIFY("end CMD test")
 
-NOTIFY("start SH test...")
 SH.CWD = "/tmp"
 SH([[
 set -efu
@@ -21,9 +19,7 @@ T["SH"] = function()
 	T.is_true(fs.isfile("/tmp/SH"))
 	os.remove("/tmp/SH")
 end
-NOTIFY("end SH test")
 
-NOTIFY("start SCRIPT test...")
 script = [[
 set -efu
 touch "/tmp/${VAR:-SCRIPT_OK}"
@@ -50,36 +46,26 @@ SCRIPT("/tmp/script.sh")
 T["SCRIPT #3"] = function()
 	T.is_string("SHOULD EXECUTE THIS TEST")
 end
-NOTIFY("end SCRIPT test")
 
-NOTIFY("start interpolation test...")
 text = "%s" % "one"
 T["interpolation #1"] = function()
 	T.equal(text, "one")
 end
-NOTIFY("end interpolation test")
 
-NOTIFY("start interpolation test...")
 text = "%s:%s" %  { "one", "two" }
 T["interpolation #2"] = function()
 	T.equal(text, "one:two")
 end
-NOTIFY("end interpolation test")
 
-NOTIFY("start env test...")
 dummy = require("lopper_dummy")
 T["environment does not cross"] = function()
 	T.is_nil(dummy.test())
 end
-NOTIFY("end env test")
 
-NOTIFY("start string metatable test...")
 dummy = require("lopper_dummy")
 T["string metatable is global"] = function()
 	T.equal(dummy.interpolation(), "yes")
 end
-NOTIFY("end string metatable test")
-
-
+NOTIFY("end Lopper tests")
 
 T.summary()
