443714703e71662dc5ac73410fdfe5ae179d908e
Eduardo Tongson <propolice@gmail.com>
2021-04-28 13:52:53 +0800

    Recents tests does not increase startup time.

---
+++ dsl.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/yuin/gopher-lua"
+)
+
+//go:embed dsl/*
+var dslSRC embed.FS
+
+func dslLoader(L *lua.LState, mod string) lua.LValue {
+	src, _ := luaSrc.ReadFile(fmt.Sprintf("dsl/%s.lua", mod))
+	fn, _ := L.LoadString(string(src))
+	return fn
+}
