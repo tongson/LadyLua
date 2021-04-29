650b78c7608bd39aa7aac656a45efe82c3e5f337
Eduardo Tongson <propolice@gmail.com>
2021-04-29 10:34:31 +0800

    split and splitv from Penlight, MIT licensed.

--- lua/string.lua
+++ lua/string.lua
@@ -2,6 +2,43 @@ do
 	local F = string.format
 	local gmatch = string.gmatch
 	local find = string.find
+	local sub = string.sub
+	local append = table.insert
+	local unpack = unpack
+
+	string.split = function(s, re, plain, n)
+		local i1, ls = 1, {}
+		if not re then
+			re = "%s+"
+		end
+		if re == "" then
+			return { s }
+		end
+		while true do
+			local i2, i3 = find(s, re, i1, plain)
+			if not i2 then
+				local last = sub(s, i1)
+				if last ~= "" then
+					append(ls, last)
+				end
+				if #ls == 1 and ls[1] == "" then
+					return {}
+				else
+					return ls
+				end
+			end
+			append(ls, sub(s, i1, i2 - 1))
+			if n and #ls == n then
+				ls[#ls] = sub(s, i1)
+				return ls
+			end
+			i1 = i3 + 1
+		end
+	end
+
+	string.splitv = function(s, re, plain, n)
+		return unpack(string.split(s, re, plain, n))
+	end
 
 	string.contains = function(str, a)
 		return find(str, a, 1, true)
