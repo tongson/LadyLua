4fdfefe4b084ff7ba1c47043ad8546d5ab9070cd
Eduardo Tongson <propolice@gmail.com>
2021-04-29 09:10:27 +0800

    Into a block for a safe scope.

--- lua/string.lua
+++ lua/string.lua
@@ -1,31 +1,33 @@
-local F = string.format
-local gmatch = string.gmatch
+do
+	local F = string.format
+	local gmatch = string.gmatch
 
-string.append = function(str, a)
-	return F("%s\n%s", str, a)
-end
-
-string.word_to_list = function(str)
-	local t = {}
-	for s in gmatch(str, "%w+") do
-		t[#t + 1] = s
+	string.append = function(str, a)
+		return F("%s\n%s", str, a)
 	end
-	return t
-end
 
-string.to_list = function(str)
-	local t = {}
-	for s in gmatch(str, "%S+") do
-		t[#t + 1] = s
+	string.word_to_list = function(str)
+		local t = {}
+		for s in gmatch(str, "%w+") do
+			t[#t + 1] = s
+		end
+		return t
 	end
-	return t
-end
 
-string.to_map = function(str, def)
-	def = def or true
-	local t = {}
-	for s in gmatch(str, "%S+") do
-		t[s] = def
+	string.to_list = function(str)
+		local t = {}
+		for s in gmatch(str, "%S+") do
+			t[#t + 1] = s
+		end
+		return t
 	end
-	return t
+
+	string.to_map = function(str, def)
+		def = def or true
+		local t = {}
+		for s in gmatch(str, "%S+") do
+			t[s] = def
+		end
+		return t
+	end
 end
