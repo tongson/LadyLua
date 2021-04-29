7f872b7f3598be5d1a0fb768a6bf781360ef060d
Eduardo Tongson <propolice@gmail.com>
2021-04-29 10:06:54 +0800

    Inside a block for safe scoping.

--- lua/fmt.lua
+++ lua/fmt.lua
@@ -1,40 +1,42 @@
-local F = string.format
+do
+	local F = string.format
 
-fmt.print = function(str, ...)
-	local stdout = io.stdout
-	stdout:write(F(str, ...))
-	return stdout:flush()
-end
+	fmt.print = function(str, ...)
+		local stdout = io.stdout
+		stdout:write(F(str, ...))
+		return stdout:flush()
+	end
 
-fmt.fprint = function(file, str, ...)
-	local o = io.output()
-	io.output(file)
-	local ret, err = io.write(F(str, ...))
-	io.output(o)
-	return ret, err
-end
+	fmt.fprint = function(file, str, ...)
+		local o = io.output()
+		io.output(file)
+		local ret, err = io.write(F(str, ...))
+		io.output(o)
+		return ret, err
+	end
 
-local warnf = function(str, ...)
-	local stderr = io.stderr
-	stderr:write(F(str, ...))
-	stderr:flush()
-end
-fmt.warn = warnf
+	local warnf = function(str, ...)
+		local stderr = io.stderr
+		stderr:write(F(str, ...))
+		stderr:flush()
+	end
+	fmt.warn = warnf
 
-local panicf = function(str, ...)
-	warnf(str, ...)
-	os.exit(1)
-end
-fmt.panic = panicf
+	local panicf = function(str, ...)
+		warnf(str, ...)
+		os.exit(1)
+	end
+	fmt.panic = panicf
 
-fmt.error = function(str, ...)
-	return nil, F(str, ...)
-end
+	fmt.error = function(str, ...)
+		return nil, F(str, ...)
+	end
 
-fmt.assert = function(v, str, ...)
-	if v then
-		return true
-	else
-		panicf(str, ...)
+	fmt.assert = function(v, str, ...)
+		if v then
+			return true
+		else
+			panicf(str, ...)
+		end
 	end
 end
