560c58bcd619eb11fac9881127b932cab713e893
Eduardo Tongson <propolice@gmail.com>
2021-05-01 17:22:03 +0800

    Tests and docs for os.outbound_ip.

--- tests/os.lua
+++ tests/os.lua
@@ -20,10 +20,25 @@ local os_hostname = function()
 	T.is_function(os.hostname)
 	T.is_string(os.hostname())
 end
+--#
+--# == *os.outbound_ip*() -> _String_
+--# Get IP used for outbound connections.
+--#
+--# === Returns
+--# [width="72%"]
+--# |===
+--# |string |IP
+--# |===
+local os_outbound_ip = function()
+	T.is_function(os.outbound_ip)
+	T.is_string(os.outbound_ip())
+end
 if included then
 	return function()
 		T["os.hostname"] = os_hostname
+		T["os.outbound_ip"] = os_outbound_ip
 	end
 else
 	T["os.hostname"] = os_hostname
+	T["os.outbound_ip"] = os_outbound_ip
 end
