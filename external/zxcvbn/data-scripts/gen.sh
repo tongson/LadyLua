ee23259252ea1d07108fb731ff180e9b46dbdd16
Eduardo Tongson <propolice@gmail.com>
2021-05-04 08:20:32 +0800

    Remove auxillary files.

--- external/zxcvbn/data-scripts/gen.sh
+++
@@ -1,5 +0,0 @@
-
-python build_frequency_lists.py ../data ../frequency/lists.go
-go fmt ../frequency/lists.go
-python build_keyboard_adjacency_graphs.py ../adjacency/graphs.go
-go fmt ../adjacency/graphs.go
