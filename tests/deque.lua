1cc4d435806a6a719670c71db32576f0219ba34a
Eduardo Tongson <propolice@gmail.com>
2021-06-07 20:01:38 +0800

    Update tests to reflect dequeue changes.

--- tests/deque.lua
+++ tests/deque.lua
@@ -30,7 +30,7 @@ local new = function()
 	tbl(q)
 end
 --#
---# == *:push_left*(_Value_)
+--# == *:push_front*(_Value_)
 --# Push to beginning of queue.
 --#
 --# === Arguments
@@ -40,7 +40,7 @@ end
 --# |value |String, Boolean, Number, or Table
 --# |===
 --#
---# == *:length*() -> _Number_
+--# == *:size*() -> _Number_
 --# Count items in queue.
 --#
 --# === Returns
@@ -59,14 +59,14 @@ end
 --# |Type |Description
 --# |table |Array
 --# |===
-local push_left = function()
+local push_front = function()
 	local q = deque.new()
-	func(q.push_left)
-	q:push_left("3")
-	q:push_left("2")
-	q:push_left("1")
-	q:push_left("2")
-	expect(4)(q:length())
+	func(q.push_front)
+	q:push_front("3")
+	q:push_front("2")
+	q:push_front("1")
+	q:push_front("2")
+	expect(4)(q:size())
 	local t = q:contents()
 	expect("2")(t[1])
 	expect("1")(t[2])
@@ -74,7 +74,7 @@ local push_left = function()
 	expect("3")(t[4])
 end
 --#
---# == *:push_right*(_Value_)
+--# == *:push_back*(_Value_)
 --# Push to end of queue.
 --#
 --# === Arguments
@@ -83,14 +83,14 @@ end
 --# |Type |Description
 --# |value |String, Boolean, Number, or Table
 --# |===
-local push_right = function()
+local push_back = function()
 	local q = deque.new()
-	func(q.push_right)
-	q:push_right(true)
-	q:push_right(false)
-	q:push_right(true)
-	q:push_right(false)
-	expect(4)(q:length())
+	func(q.push_back)
+	q:push_back(true)
+	q:push_back(false)
+	q:push_back(true)
+	q:push_back(false)
+	expect(4)(q:size())
 	local t = q:contents()
 	expect(true)(t[1])
 	expect(false)(t[2])
@@ -98,7 +98,7 @@ local push_right = function()
 	expect(false)(t[4])
 end
 --#
---# == *:pop_left*() -> _Value_
+--# == *:pop_front*() -> _Value_
 --# Pop value from beginning of queue.
 --#
 --# === Returns
@@ -107,26 +107,26 @@ end
 --# |Type |Description
 --# |value |String, Boolean, Number, or Table
 --# |===
-local pop_left = function()
+local pop_front = function()
 	local q = deque.new()
-	func(q.pop_left)
-	q:push_right({ "1" })
-	q:push_right({ "2" })
-	q:push_right({ "3" })
-	q:push_right({ "4" })
-	expect(4)(q:length())
-	local t1 = q:pop_left()
+	func(q.pop_front)
+	q:push_back({ "1" })
+	q:push_back({ "2" })
+	q:push_back({ "3" })
+	q:push_back({ "4" })
+	expect(4)(q:size())
+	local t1 = q:pop_front()
 	expect("1")(t1[1])
-	local t2 = q:pop_left()
+	local t2 = q:pop_front()
 	expect("2")(t2[1])
-	local t3 = q:pop_left()
+	local t3 = q:pop_front()
 	expect("3")(t3[1])
-	local t4 = q:pop_left()
+	local t4 = q:pop_front()
 	expect("4")(t4[1])
-	expect(0)(q:length())
+	expect(0)(q:size())
 end
 --#
---# == *:pop_right*() -> _Value_
+--# == *:pop_back*() -> _Value_
 --# Pop value from end of queue.
 --#
 --# === Returns
@@ -135,22 +135,22 @@ end
 --# |Type |Description
 --# |value |String, Boolean, Number, or Table
 --# |===
-local pop_right = function()
+local pop_back = function()
 	local q = deque.new()
-	func(q.pop_right)
-	q:push_right(1)
-	q:push_right(2)
-	q:push_right(3)
-	q:push_right(4)
-	expect(4)(q:length())
-	expect(4)(q:pop_right())
-	expect(3)(q:pop_right())
-	expect(2)(q:pop_right())
-	expect(1)(q:pop_right())
-	expect(0)(q:length())
+	func(q.pop_back)
+	q:push_back(1)
+	q:push_back(2)
+	q:push_back(3)
+	q:push_back(4)
+	expect(4)(q:size())
+	expect(4)(q:pop_back())
+	expect(3)(q:pop_back())
+	expect(2)(q:pop_back())
+	expect(1)(q:pop_back())
+	expect(0)(q:size())
 end
 --#
---# == *:peek_left*() -> _Value_
+--# == *:peek_front*() -> _Value_
 --# Return first value in the queue.
 --#
 --# === Returns
@@ -159,20 +159,20 @@ end
 --# |Type |Description
 --# |value |String, Boolean, Number, or Table
 --# |===
-local peek_left = function()
+local peek_front = function()
 	local q = deque.new()
-	func(q.peek_left)
-	q:push_right({ "1" })
-	q:push_right({ "2" })
-	q:push_right({ "3" })
-	q:push_right({ "4" })
-	expect(4)(q:length())
-	local t1 = q:peek_left()
+	func(q.peek_front)
+	q:push_back({ "1" })
+	q:push_back({ "2" })
+	q:push_back({ "3" })
+	q:push_back({ "4" })
+	expect(4)(q:size())
+	local t1 = q:peek_front()
 	expect("1")(t1[1])
-	expect(4)(q:length())
+	expect(4)(q:size())
 end
 --#
---# == *:peek_right*() -> _Value_
+--# == *:peek_back*() -> _Value_
 --# Return last value in queue.
 --#
 --# === Returns
@@ -181,19 +181,19 @@ end
 --# |Type |Description
 --# |value |String, Boolean, Number, or Table
 --# |===
-local peek_right = function()
+local peek_back = function()
 	local q = deque.new()
-	func(q.peek_right)
-	q:push_right(1)
-	q:push_right(2)
-	q:push_right(3)
-	q:push_right(4)
-	expect(4)(q:length())
-	expect(4)(q:peek_right())
-	expect(4)(q:length())
+	func(q.peek_back)
+	q:push_back(1)
+	q:push_back(2)
+	q:push_back(3)
+	q:push_back(4)
+	expect(4)(q:size())
+	expect(4)(q:peek_back())
+	expect(4)(q:size())
 end
 --#
---# == *:remove_left*(_Value_) -> _Boolean_
+--# == *:remove_front*(_Value_) -> _Boolean_
 --# Remove first matching value from beginning of queue.
 --#
 --# === Arguments
@@ -209,18 +209,18 @@ end
 --# |Type |Description
 --# |boolean |`true` if removed, `false` otherwise
 --# |===
-local remove_left = function()
+local remove_front = function()
 	local q = deque.new()
-	func(q.remove_left)
-	q:push_right(1)
-	q:push_right(2)
-	q:push_right(2)
-	q:push_right(4)
-	q:push_right(2)
-	expect(5)(q:length())
-	local b = q:remove_left(2)
+	func(q.remove_front)
+	q:push_back(1)
+	q:push_back(2)
+	q:push_back(2)
+	q:push_back(4)
+	q:push_back(2)
+	expect(5)(q:size())
+	local b = q:remove_front(2)
 	expect(true)(b)
-	expect(4)(q:length())
+	expect(4)(q:size())
 	local t = q:contents()
 	expect(1)(t[1])
 	expect(2)(t[2])
@@ -228,7 +228,7 @@ local remove_left = function()
 	expect(2)(t[4])
 end
 --#
---# == *:remove_right*(_Value_) -> _Boolean_
+--# == *:remove_back*(_Value_) -> _Boolean_
 --# Remove first matching value from end of queue.
 --#
 --# === Arguments
@@ -244,18 +244,18 @@ end
 --# |Type |Description
 --# |boolean |`true` if removed, `false` otherwise
 --# |===
-local remove_right = function()
+local remove_back = function()
 	local q = deque.new()
-	func(q.remove_right)
-	q:push_right(1)
-	q:push_right(2)
-	q:push_right(2)
-	q:push_right(4)
-	q:push_right(2)
-	expect(5)(q:length())
-	local b = q:remove_right(2)
+	func(q.remove_back)
+	q:push_back(1)
+	q:push_back(2)
+	q:push_back(2)
+	q:push_back(4)
+	q:push_back(2)
+	expect(5)(q:size())
+	local b = q:remove_back(2)
 	expect(true)(b)
-	expect(4)(q:length())
+	expect(4)(q:size())
 	local t = q:contents()
 	expect(1)(t[1])
 	expect(2)(t[2])
@@ -276,20 +276,20 @@ local is_empty = function()
 	local q = deque.new()
 	func(q.is_empty)
 	expect(true)(q:is_empty())
-	q:push_right("x")
+	q:push_back("x")
 	expect(false)(q:is_empty())
 end
 --#
---# == *:iter_left*()
+--# == *:iter_front*()
 --# Iterate from start of queue.
-local iter_left = function()
+local iter_front = function()
 	local q = deque.new()
-	func(q.iter_left)
-	q:push_right(1)
-	q:push_right(true)
-	q:push_right({})
+	func(q.iter_front)
+	q:push_back(1)
+	q:push_back(true)
+	q:push_back({})
 	local t = {}
-	for x in q:iter_left() do
+	for x in q:iter_front() do
 		t[#t+1] = x
 	end
 	expect(1)(t[1])
@@ -297,16 +297,16 @@ local iter_left = function()
 	tbl(t[3])
 end
 --#
---# == *:iter_right*()
+--# == *:iter_back*()
 --# Iterate from end of queue.
-local iter_right = function()
+local iter_back = function()
 	local q = deque.new()
-	func(q.iter_right)
-	q:push_right(1)
-	q:push_right(true)
-	q:push_right({})
+	func(q.iter_back)
+	q:push_back(1)
+	q:push_back(true)
+	q:push_back({})
 	local t = {}
-	for x in q:iter_right() do
+	for x in q:iter_back() do
 		t[#t+1] = x
 	end
 	tbl(t[1])
@@ -314,7 +314,7 @@ local iter_right = function()
 	expect(1)(t[3])
 end
 --#
---# == *:rotate_left*([_Number_])
+--# == *:rotate_front*([_Number_])
 --# Rotate queue from beginning. Argument is number of steps to rotate, defaults to 1.
 --#
 --# === Example
@@ -334,25 +334,25 @@ end
 --# |Type |Description
 --# |number |Rotation steps
 --# |===
-local rotate_left = function()
+local rotate_front = function()
 	local q = deque.new()
-	func(q.rotate_left)
-	q:push_right(4)
-	q:push_right(2)
-	q:push_right(3)
-	q:rotate_left()
+	func(q.rotate_front)
+	q:push_back(4)
+	q:push_back(2)
+	q:push_back(3)
+	q:rotate_front()
 	local t = q:contents()
 	expect(2)(t[1])
 	expect(3)(t[2])
 	expect(4)(t[3])
-	q:rotate_left(3)
+	q:rotate_front(3)
 	local y = q:contents()
 	expect(2)(y[1])
 	expect(3)(y[2])
 	expect(4)(y[3])
 end
 --#
---# == *:rotate_right*([_Number_])
+--# == *:rotate_back*([_Number_])
 --# Rotate queue from end. Argument is number of steps to rotate, defaults to 1.
 --#
 --# === Example
@@ -370,18 +370,18 @@ end
 --# |Type |Description
 --# |number |Rotation steps
 --# |===
-local rotate_right = function()
+local rotate_back = function()
 	local q = deque.new()
-	func(q.rotate_right)
-	q:push_right(2)
-	q:push_right(3)
-	q:push_right(4)
-	q:rotate_right()
+	func(q.rotate_back)
+	q:push_back(2)
+	q:push_back(3)
+	q:push_back(4)
+	q:rotate_back()
 	local t = q:contents()
 	expect(4)(t[1])
 	expect(2)(t[2])
 	expect(3)(t[3])
-	q:rotate_right(1)
+	q:rotate_back(1)
 	local y = q:contents()
 	expect(3)(y[1])
 	expect(4)(y[2])
@@ -389,34 +389,34 @@ local rotate_right = function()
 end
 if included then
 	return function()
-		T["push_left"] = push_left
-		T["push_right"] = push_right
-		T["pop_left"] = pop_left
-		T["pop_right"] = pop_right
-		T["peek_left"] = peek_left
-		T["peek_right"] = peek_right
-		T["remove_left"] = remove_left
-		T["remove_right"] = remove_right
+		T["push_front"] = push_front
+		T["push_back"] = push_back
+		T["pop_front"] = pop_front
+		T["pop_back"] = pop_back
+		T["peek_front"] = peek_front
+		T["peek_back"] = peek_back
+		T["remove_front"] = remove_front
+		T["remove_back"] = remove_back
 		T["is_empty"] = is_empty
-		T["iter_left"] = iter_left
-		T["iter_right"] = iter_right
-		T["rotate_left"] = rotate_left
-		T["rotate_right"] = rotate_right
+		T["iter_front"] = iter_front
+		T["iter_back"] = iter_back
+		T["rotate_front"] = rotate_front
+		T["rotate_back"] = rotate_back
 	end
 else
-	T["push_left"] = push_left
-	T["push_right"] = push_right
-	T["pop_left"] = pop_left
-	T["pop_right"] = pop_right
-	T["peek_left"] = peek_left
-	T["peek_right"] = peek_right
-	T["remove_left"] = remove_left
-	T["remove_right"] = remove_right
+	T["push_front"] = push_front
+	T["push_back"] = push_back
+	T["pop_front"] = pop_front
+	T["pop_back"] = pop_back
+	T["peek_front"] = peek_front
+	T["peek_back"] = peek_back
+	T["remove_front"] = remove_front
+	T["remove_back"] = remove_back
 	T["is_empty"] = is_empty
-	T["iter_left"] = iter_left
-	T["iter_right"] = iter_right
-	T["rotate_left"] = rotate_left
-	T["rotate_right"] = rotate_right
+	T["iter_front"] = iter_front
+	T["iter_back"] = iter_back
+	T["rotate_front"] = rotate_front
+	T["rotate_back"] = rotate_back
 	local queue = deque.new()
 	local round = 0
 	local push_mean = 0
@@ -427,14 +427,14 @@ else
 	max_count = 1000 * 1000 * 10
 	o = os.clock()
 	for i = 50, max_count + 50, 1 do
-		queue:push_left(i)
+		queue:push_front(i)
 	end
 	t = os.clock() - o
 	push_mean = push_mean + t
 	print("push " .. max_count .. " cost " .. t)
 	o = os.clock()
-	while queue:length() > 0 do
-		queue:remove_right()
+	while queue:size() > 0 do
+		queue:remove_back()
 	end
 	t = os.clock() - o
 	pop_mean = pop_mean + t
