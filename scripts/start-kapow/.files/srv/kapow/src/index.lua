42065f64ede7912777d0ccd488d3a9d985996965
Eduardo Tongson <propolice@gmail.com>
2021-06-10 19:28:24 +0800

    No longer needed.

--- scripts/start-kapow/.files/srv/kapow/src/index.lua
+++
@@ -1,2 +0,0 @@
-dofile '/srv/kapow/src/config.lua'
-require('handler')()
--- scripts/start-redis/.files/__ll/start-redis.lua
+++
@@ -1,4 +0,0 @@
-local systemctl = exec.cmd 'systemctl'
-systemctl 'stop redis@default.service'
-systemctl.errexit = true
-systemctl 'enable --now redis@default.service'
