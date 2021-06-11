8e4f0ed112e19a94024a9bbfb9ba4db8cce84bac
Eduardo Tongson <propolice@gmail.com>
2021-06-11 19:09:15 +0800

    Rearrange for easier removal of unneeded modules.

--- cmd/standalone/main.go
+++ cmd/standalone/main.go
@@ -22,17 +22,29 @@ func main() {
 	defer ll.RecoverPanic()
 	L := lua.NewState()
 	defer L.Close()
+	// fs
 	nsFs := L.SetFuncs(L.NewTable(), lfs.Api)
 	L.SetGlobal("fs", nsFs)
 	L.SetField(nsFs, "isdir", L.NewFunction(ll.FsIsdir))
 	L.SetField(nsFs, "isfile", L.NewFunction(ll.FsIsfile))
 	L.SetField(nsFs, "read", L.NewFunction(ll.FsRead))
 	L.SetField(nsFs, "write", L.NewFunction(ll.FsWrite))
+	// fs
+	// os
 	nsOs := L.GetField(L.Get(lua.EnvironIndex), "os")
 	L.SetField(nsOs, "hostname", L.NewFunction(ll.OsHostname))
 	L.SetField(nsOs, "outbound_ip", L.NewFunction(ll.OsOutboundIP))
 	L.SetField(nsOs, "sleep", L.NewFunction(ll.OsSleep))
+	// os
+	// pi
 	L.SetGlobal("pi", L.NewFunction(ll.GlobalPi))
+	// pi
+	// exec
+	L.SetGlobal("exec", L.NewTable())
+	nsExec := L.GetField(L.Get(lua.EnvironIndex), "exec")
+	L.SetField(nsExec, "command", L.NewFunction(ll.ExecCommand))
+	ll.PatchLoader(L, "exec")
+	// exec
 	L.PreloadModule("http", gluahttp.Xloader)
 	L.PreloadModule("ll_json", ljson.Loader)
 	L.PreloadModule("crypto", gluacrypto.Loader)
@@ -49,13 +61,9 @@ func main() {
 	L.PreloadModule("rr", ll.RrLoader)
 	L.PreloadModule("uuid", ll.UuidLoader)
 	L.PreloadModule("ulid", ll.UlidLoader)
-	L.SetGlobal("exec", L.NewTable())
-	nsExec := L.GetField(L.Get(lua.EnvironIndex), "exec")
-	L.SetField(nsExec, "command", L.NewFunction(ll.ExecCommand))
-	ll.PatchLoader(L, "exec")
+	L.PreloadModule("redis", ll.RedisLoader)
 	ll.PatchLoader(L, "table")
 	ll.PatchLoader(L, "string")
-	L.PreloadModule("redis", ll.RedisLoader)
 	preload := L.GetField(L.GetField(L.Get(lua.EnvironIndex), "package"), "preload")
 	L.SetField(preload, "json", ll.LuaLoader(L, "json"))
 	L.SetField(preload, "list", ll.LuaLoader(L, "list"))
