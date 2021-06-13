c0848314776103481a50e603593b281718aac5cd
Eduardo Tongson <propolice@gmail.com>
2021-06-13 19:45:25 +0800

    Helper routine for strings read from embed.FS.

--- src/helper.go
+++ src/helper.go
@@ -1,7 +1,10 @@
 package ll
 
 import (
+	"embed"
+	"fmt"
 	"github.com/yuin/gopher-lua"
+	"os"
 )
 
 func FillArg(L *lua.LState, args []string) {
@@ -10,4 +13,13 @@ func FillArg(L *lua.LState, args []string) {
 		L.RawSet(argtb, lua.LNumber(i), lua.LString(args[i]))
 	}
 	L.SetGlobal("arg", argtb)
+}
+
+func ReadFile(f embed.FS, p string) string {
+	c, err := f.ReadFile(p)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(c)
 }
