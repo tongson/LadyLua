36c99fbe3818923ad57675aa59e6942d8e1d4e24
Eduardo Tongson <propolice@gmail.com>
2021-06-14 10:08:03 +0800

    Document helper functions.

--- src/helper.go
+++ src/helper.go
@@ -7,6 +7,16 @@ import (
 	"os"
 )
 
+//#
+//# == *ll.FillArg*(*lua.LState, []string)
+//# Capture command line arguments as the `arg` table in the global `_G` environment.
+//#
+//# === Arguments
+//# [width="72%"]
+//# |===
+//# |*lua.LState|The current `LState`; usually the result of `lua.NewState()`
+//# |[]string |Usually `os.Args`
+//# |===
 func FillArg(L *lua.LState, args []string) {
 	argtb := L.NewTable()
 	for i := 0; i < len(args); i++ {
@@ -15,6 +25,22 @@ func FillArg(L *lua.LState, args []string) {
 	L.SetGlobal("arg", argtb)
 }
 
+//#
+//# == *ll.ReadFile*(embed.FS, string) -> string
+//# Read file from an `embed.FS` location.
+//#
+//# === Arguments
+//# [width="72%"]
+//# |===
+//# |embed.FS |Variable of embedded filesystem
+//# |string |Filename
+//# |===
+//#
+//# === Returns
+//# [width="72%"]
+//# |===
+//# |string |Contents of file
+//# |===
 func ReadFile(f embed.FS, p string) string {
 	c, err := f.ReadFile(p)
 	if err != nil {
