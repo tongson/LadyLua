c1e58200b1ae7977f3be431c37b04bafada9a627
Eduardo Tongson <propolice@gmail.com>
2021-06-18 13:24:18 +0800

    No longer works with PatchLoader.

--- cmd/standalone/main.go
+++ cmd/standalone/main.go
@@ -22,8 +22,6 @@ func main() {
 	ll.GlobalLoader(L, "pi")
 	ll.GlobalLoader(L, "exec")
 	ll.PatchLoader(L, "exec")
-	ll.PatchLoader(L, "table")
-	ll.PatchLoader(L, "string")
 	ll.GoLoader(L, "http")
 	ll.GoLoader(L, "json")
 	ll.GoLoader(L, "crypto")
