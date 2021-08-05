5748fcf43d8153b441b9ffdf33edc3ca4eae5aa1
Eduardo Tongson <propolice@gmail.com>
2021-08-05 13:27:57 +0800

    Test new TOML support.

--- tests/refmt.lua
+++ tests/refmt.lua
@@ -2,6 +2,7 @@ local included = pcall(debug.getlocal, 4, 1)
 local T = require 'test'
 local refmt = require("refmt")
 local json = require("json")
+local expect = T.expect
 --# = refmt
 --# :toc:
 --# :toc-placement!:
@@ -62,13 +63,78 @@ local refmt_json = function()
 	local expected = [[{"value":"hello"}]]
   T.equal(refmt.json(y), expected)
 end
+--#
+--# == *refmt.toml_to_json*(_String_) -> _String_
+--# Convert TOML to JSON.
+--#
+--# === Arguments
+--# [options="header",width="72%"]
+--# |===
+--# |Type |Description
+--# |string |TOML
+--# |===
+--#
+--# === Returns
+--# [options="header",width="72%"]
+--# |===
+--# |Type |Description
+--# |string |JSON
+--# |===
+local refmt_toml_json = function()
+	T.is_function(refmt.toml_to_json)
+	local t = [[
+	[mytoml]
+	a = 42
+	]]
+	local expected = [[{
+  "mytoml": {
+    "a": 42
+  }
+}]]
+	T.equal(refmt.toml_to_json(t), expected)
+end
+--#
+--# == *refmt.json_to_toml*(_String_) -> _String_
+--# Convert JSON to TOML.
+--#
+--# === Arguments
+--# [options="header",width="72%"]
+--# |===
+--# |Type |Description
+--# |string |JSON
+--# |===
+--#
+--# === Returns
+--# [options="header",width="72%"]
+--# |===
+--# |Type |Description
+--# |string |TOML
+--# |===
+local refmt_json_toml = function()
+	T.is_function(refmt.json_to_toml)
+	local expected = [[
+
+[mytoml]
+  a = 42.0
+]]
+	local j = [[{
+  "mytoml": {
+    "a": 42
+  }
+}]]
+	expect(expected)(refmt.json_to_toml(j))
+end
 if included then
   return function()
 		T["refmt.yaml"] = refmt_yaml
 		T["refmt.json"] = refmt_json
+		T["refmt.toml_to_json"] = refmt_toml_json
+		T["refmt.json_to_toml"] = refmt_json_toml
   end
 else
 
 		T["refmt.yaml"] = refmt_yaml
 		T["refmt.json"] = refmt_json
+		T["refmt.toml_to_json"] = refmt_toml_json
+		T["refmt.json_to_toml"] = refmt_json_toml
 end
